eb65fddb0d481dfaf59405a5b21da37a19e3316a
M Haidar Hanif <git@mhaidarhanif.com>
2018-03-19 16:25:20 +0700

    Refine install script

--- chapter-1/module-setup-1/INSTALL.sh
+++ chapter-1/module-setup-1/INSTALL.sh
@@ -2,16 +2,15 @@
 
 ## Install essential tools
 
-sudo apt install zsh vim git tig tree curl wget httpie imagemagick svgo
-
-## Install optional tools
-
-sudo apt install vlc
+sudo apt install zsh vim git tig tree curl wget httpie imagemagick vlc
 
 ## Install browser
 
 # wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome-stable_current_amd64.deb
+
+# wget https://atom-installer.github.com/v1.25.0/atom-amd64.deb?s=1521055093&ext=.deb
+sudo dpkg -i atom-amd64.deb
 
 # ------------------------------------------------------------------------------
 
@@ -24,6 +23,7 @@ sudo dpkg -i google-chrome-stable_current_amd64.deb
 brew install zsh vim git tig tree curl wget httpie
 
 brew cask install google-chrome
+brew cask install atom
 
 # ------------------------------------------------------------------------------
 
